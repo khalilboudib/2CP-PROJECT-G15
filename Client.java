9e62005b951054e3b196a8ee4b4771f9e12d4b16
yacineallag19 <154988480+yacineallag19@users.noreply.github.com>
2024-03-01 15:31:04 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,99 +1,91 @@
-import java.sql.*;
+import java.sql.Date;
 
-public class MyJDBC {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cp2_project";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "cp2_project";
+public class Client {
 
-    // Establish a connection to the database
-    public static Connection connectToDataBase() throws SQLException, ClassNotFoundException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Load MySQL JDBC driver
-            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD); // Connect to the database using the provided URL, username, and password
-        } catch (ClassNotFoundException e) {
-            System.err.println("Failed to load MySQL JDBC driver."); // If the JDBC driver class is not found, print an error message
-            throw e; // Rethrow the exception to notify the caller about the failure
-        } catch (SQLException e) {
-            System.err.println("Failed to connect to the database."); // If a SQL exception occurs during connection, print an error message
-            throw e; // Rethrow the exception to notify the caller about the failure
-        }
+    private long cardNumber ;
+    private String expiringDate ;
+    private String firstName;
+
+    private String lastName;
+
+    private String userAdress ;
+
+
+    private byte[] publicKey;
+
+    private byte[] serverPrivateKey ;
+
+    public Client (Long cardNumber , String expiringDate , String firstName , String lastName , String userAdress) {
+        this.cardNumber = cardNumber ;
+        this.expiringDate = expiringDate ;
+        this.firstName = firstName ;
+        this.lastName = lastName ;
+        this.userAdress = userAdress ;
+        publicKey = new byte[256];
+        for (int i = 0; i < publicKey.length; i++) publicKey[i] = (byte) i;
     }
 
 
-    // Search for client and retreive his record
-    public static boolean searchClient(Connection connection, Client client, ResultSet foundClient) throws SQLException {
-        String sqlQuery = "Select * from clients where card_number= ? ";
-        try (PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
-            statement.setLong(1,client.getCardNumber()); // Set the placeholder to client card number
-            foundClient = statement.executeQuery(); // Execute the update to add the client to the database
-            if ( foundClient == null ) {
-                return false ; // Not Found
-            }
-            else {
-                return true ; // Found
-            }
-        } catch (SQLException e) {
-            System.err.println("Error finding client: " + e.getMessage()); // If a SQL exception occurs during client search, print an error message
-            throw e ;
-        }
+
+
+
+    public void setCardNumber(long cardNumber) {
+        this.cardNumber = cardNumber;
     }
 
-    // Add a client to the database
-    public static void addClient(Connection connection, Client client) throws SQLException {
-        String sqlQuery = "INSERT INTO clients (card_number, card_expiring_date, first_name, last_name, user_adress, public_key) VALUES (?, ?, ?, ?, ?, ? , ?)";
-        try (PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
-            // Set the statement parameters for the client data
-            setStatement(statement, client);
-            // Execute the update to add the client to the database
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            // If a SQL exception occurs during client addition, print an error message
-            System.err.println("Error adding client: " + e.getMessage());
-            throw e ;
-        }
+    public long getCardNumber() {
+        return cardNumber ;
     }
 
-    // Set the statement parameters for adding a client
-    public static void setStatement(PreparedStatement statement, Client client) throws SQLException {
-        try {
-            // Set each parameter of the prepared statement with client data
-            statement.setLong(1, client.getCardNumber());
-            statement.setDate(2, Date.valueOf(client.getExpiringDate()));
-            statement.setString(3, client.getFirstName());
-            statement.setString(4, client.getLastName());
-            statement.setString(5, client.getUserAdress());
-            statement.setBytes(6, client.getPublicKey());
-            statement.setBytes(7,client.getServerPrivateKey());
-        } catch (SQLException e) {
-            System.err.println("Error setting statement: " + e.getMessage()); // If a SQL exception occurs during parameter setting, print an error message
-            throw e; // Rethrow the exception to propagate it
-        }
+    public void setExpiringDate(String expiringDate) {
+        this.expiringDate = expiringDate;
+    }
+
+    public String getExpiringDate() {
+        return expiringDate ;
     }
 
 
-    // Delete client from data base
-    public static void deleteClient (Connection connection , Client client ) throws SQLException {
-        String sqlQuery = "delete from clients where card_number= ? ";
-        try (PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
-            statement.setLong(1,client.getCardNumber()); // Set the placeholder to client card number
-            statement.executeUpdate(); // Execute the update to delete the client from the database
-        } catch (SQLException e) {
-            System.err.println("Error deleting client: " + e.getMessage()); // If a SQL exception occurs during client delete, print an error message
-            throw e ;
-        }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
 
-    public static void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-                System.out.println("Connection closed successfully.");
-            } catch (SQLException e) {
-                System.err.println("Error closing connection: " + e.getMessage());
-            }
-        }
+    public String getLastName() {
+        return lastName;
     }
 
+    public void setUserAdress(String userAdress) {
+        this.userAdress = userAdress;
+    }
+
+    public String getUserAdress() {
+        return userAdress;
+    }
+
+    public void setPublicKey(byte[] publicKey) {
+        this.publicKey = publicKey;
+    }
+
+
+    public byte[] getPublicKey() {
+        return publicKey;
+    }
+
+    public void setServerPrivateKey(byte[] serverPrivateKey) {
+        this.serverPrivateKey = serverPrivateKey;
+    }
+
+
+    public byte[] getServerPrivateKey() {
+        return serverPrivateKey;
+    }
 
 }
