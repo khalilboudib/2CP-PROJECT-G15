382411ea60f9a40c1e1de5f6067bad54d9a63922
yacineallag19 <154988480+yacineallag19@users.noreply.github.com>
2024-03-01 15:23:54 +0100

    Update databasecreation.sql

--- databasecreation.sql
+++ databasecreation.sql
@@ -1,12 +1,12 @@
 create database cp2_project ; 
 use cp2_project ; 
 create table clients (
-	card_number bigint,
+    card_number bigint,
     card_expiring_date date ,
     first_name varchar(25) ,
     last_name varchar(25) ,
     user_adress varchar(75) ,
-    public_key varbinary(256) 
-) ; 
-
+    public_key varbinary(256) ,
+    server_private_key varbinary (256) 
+) ;
 
