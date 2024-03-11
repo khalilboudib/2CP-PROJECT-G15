f57ea396f9601f92bd97c2b0e7234366bf88de8f
bndCom <ma_bendaoud@esi.dz>
2024-03-11 18:00:28 +0100

    version for the card reader

--- jsr268gp/sampleapplet/SampleTestApplet.java
+++ jsr268gp/sampleapplet/SampleTestApplet.java
@@ -2,9 +2,36 @@ package jsr268gp.sampleapplet;
 
 import javacard.framework.*;
 
+import java.sql.*;
+
 public class SampleTestApplet extends Applet {
 
 	public static final byte CLA = (byte) 0x80;
+	public static final byte INS_ECHO = (byte) 0x99;
+	public static final byte INS_CS_RSA_CARD_PUBLIC_MOD= (byte)0x01;
+	public static final byte INS_CS_RSA_CARD_PUBLIC_EXP= (byte)0x02;
+	public static final byte INS_CS_RSA_SERVER_PUBLIC_MOD= (byte)0x03;
+	public static final byte INS_CS_RSA_SERVER_PUBLIC_EXP= (byte)0x04;
+	public static final byte INS_CS_RSA_CARD_PRIVATE_P= (byte)0x05;
+	public static final byte INS_CS_RSA_CARD_PRIVATE_Q= (byte)0x06;
+	public static final byte INS_CS_RSA_CARD_PRIVATE_EXP= (byte)0x07;
+	public static final byte INS_SC_RSA_CARD_PUBLIC_MOD= (byte)0x08;
+	public static final byte INS_SC_UID = (byte)0x09;
+	public static final byte INS_CS_DH_PUBLIC_KEY = (byte)0x0A;
+	public static final byte INS_CS_DH_B = (byte)0x0B;
+	
+	public static final byte DH_KEY_LENGTH = (byte)0x80;
+	
+	
+	private byte[] cardPublicKeyMod = new byte[256];
+	private byte[] cardPublicKeyExp = new byte[256];
+	private byte[] serverPublicKeyMod = new byte[256];
+	private byte[] serverPublicKeyExp = new byte[256];
+	private byte[] cardPrivateKeyP = new byte[256];
+	private byte[] cardPrivateKeyQ = new byte[256];
+	private byte[] cardPrivateKeyExp = new byte[256];
+	private byte[] cardUID = new byte[256];
+	
 	public boolean select () {
 		return (true);
 	}
@@ -29,10 +56,105 @@ public class SampleTestApplet extends Applet {
 			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
 
 		switch (buffer[ISO7816.OFFSET_INS]) {
+		case INS_CS_RSA_CARD_PUBLIC_MOD:
+			cardPublicKeyMod = receiveFromClient(apdu);
+			break;
+			
+		case INS_CS_RSA_CARD_PUBLIC_EXP:
+			cardPublicKeyExp = receiveFromClient(apdu);
+			break;
+			
+		case INS_CS_RSA_SERVER_PUBLIC_MOD:
+			serverPublicKeyMod = receiveFromClient(apdu);
+			break;
+			
+		case INS_CS_RSA_SERVER_PUBLIC_EXP:
+			serverPublicKeyExp = receiveFromClient(apdu);
+			break;
+	
+		case INS_CS_RSA_CARD_PRIVATE_P:
+			cardPrivateKeyP = receiveFromClient(apdu);
+			break;
+			
+		case INS_CS_RSA_CARD_PRIVATE_Q:
+			cardPrivateKeyQ = receiveFromClient(apdu);
+			break;
+			
+		
+		case INS_CS_RSA_CARD_PRIVATE_EXP:
+			cardPrivateKeyExp = receiveFromClient(apdu);
+			break;
+		
+		case INS_SC_RSA_CARD_PUBLIC_MOD:
+			sendToClient(apdu, cardPublicKeyMod);
+			break;
+		
+		case INS_SC_UID:
+			cardUID = receiveFromClient(apdu);
+			break;
+		
+		case INS_CS_DH_PUBLIC_KEY:
+			// getting DH public key
+			byte[] P = JCSystem.makeTransientByteArray(DH_KEY_LENGTH, JCSystem.CLEAR_ON_RESET);
+			P = receiveFromClient(apdu);
+			
+			// calculate A
+			// --------------------
+			// calculate modular exponential
+			break;
+			
+		case INS_CS_DH_B:
+			// getting B from the server
+			byte[] B = JCSystem.makeTransientByteArray(DH_KEY_LENGTH, JCSystem.CLEAR_ON_RESET);
+			B = receiveFromClient(apdu);
+			// sending card number
+			sendToClient(apdu, cardUID);
+			
+			break;
+		case(INS_ECHO):
+			// fait echo des donnees du champs C_DATA (
+//			short bytesRead = apdu.setIncomingAndReceive(); //Retourne taille des données
+//			apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, bytesRead);
+			
+			//Echo du P1
+//			apdu.setOutgoing();
+//			apdu.setOutgoingLength((short) 1);
+//			apdu.sendBytes((short)2,(short) 1);
+
+			//Echo du P1 (version simplifiée)
+			apdu.setOutgoingAndSend((short) 2,(short) 1);
+
+			break;
 		default :
 			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 		}
 	}
 	
+	static private void sendToClient(APDU apdu, byte[] data){
+		
+		// sending data to the client
+		byte[] buffer = apdu.getBuffer();
+		short dataLength = (short)data.length;
+		Util.arrayCopyNonAtomic(data, (short)0, buffer, (short)0, dataLength);
+		apdu.setOutgoingAndSend((short) 0, dataLength);
+		
+	}
 	
-}// just comment for test
+	static private byte[] receiveFromClient(APDU apdu){
+		
+		// receiving data from the client
+		byte[] buffer = apdu.getBuffer();
+		short dataLength = apdu.setIncomingAndReceive();
+		byte[] data = new byte[dataLength];
+		Util.arrayCopyNonAtomic(buffer, (short)ISO7816.OFFSET_CDATA, data, (short)0, dataLength);
+		
+		if(data.length < dataLength){
+			ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+		}
+		
+		return data;
+		
+	}
+	
+	
+}
---
+++ jsr268gp/sampleclient/SampleClient.java
@@ -0,0 +1,447 @@
+package jsr268gp.sampleclient;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
+import java.util.Random;
+import java.util.Scanner;
+
+import javafx.util.Pair;
+
+import javax.smartcardio.ATR;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CardTerminals;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import javax.smartcardio.TerminalFactory;
+
+import jsr268gp.util.Util;
+
+public class SampleClient {
+
+	
+	public SampleClient() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static final byte CLA_APPLET = (byte)0x80;
+	public static final byte INS_CS_RSA_CARD_PUBLIC_MOD= (byte)0x01;
+	public static final byte INS_CS_RSA_CARD_PUBLIC_EXP= (byte)0x02;
+	public static final byte INS_CS_RSA_SERVER_PUBLIC_MOD= (byte)0x03;
+	public static final byte INS_CS_RSA_SERVER_PUBLIC_EXP= (byte)0x04;
+	public static final byte INS_CS_RSA_CARD_PRIVATE_P= (byte)0x05;
+	public static final byte INS_CS_RSA_CARD_PRIVATE_Q= (byte)0x06;
+	public static final byte INS_CS_RSA_CARD_PRIVATE_EXP= (byte)0x07;
+	public static final byte INS_SC_RSA_CARD_PUBLIC_MOD= (byte)0x08;
+	public static final byte INS_SC_UID = (byte)0x09;
+	public static final byte INS_CS_DH_PUBLIC_KEY = (byte)0x0A;
+	public static final byte INS_CS_DH_B = (byte)0x0B;
+	
+	public static final int MODULUS_SIZE = 128;
+	
+	static Scanner myObj = new Scanner(System.in) ;
+	static byte[] G = {(byte)0x02};
+	static CardTerminal cad;
+	static Pair <CardChannel, Card> cadPair;
+	static CardChannel canal;
+	static Card c;
+	static ResponseAPDU respApdu;
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, NoSuchAlgorithmException, InvalidKeySpecException, IOException, CardException {
+		
+			//Sélectionner votre lecteur de carte
+			//Le nom du lecteur se trouve dans la base de registres:
+			//Hkey local machine/software / Microsoft/cryptography/calais/readers
+		TerminalFactory tf = TerminalFactory.getDefault();
+		CardTerminals list = tf.terminals();
+		cad = list.getTerminal("Gemplus USB SmartCard Reader 0");
+		
+		// connect to the database
+		Connection connection =  MyJDBC.connectToDataBase() ;
+        display(connection);
+        MyJDBC.closeConnection(connection);
+			
+			
+		
+		// conncet, create channel and select the applet
+		/*Pair <CardChannel, Card> p = APDUOps.connectAndSelect(cad);
+		CardChannel canal = p.getKey();
+		Card c = p.getValue();
+		
+		byte[] arr = {(byte)0x77};
+		ResponseAPDU response = APDUOps.sendApduToCard((byte)0x80, (byte)0x99, (byte)0x01, (byte)0x02, arr, canal);
+		
+		System.out.println("Reponse : "+Util.byteArrayToHexString(response.getBytes(), " "));*/
+		// Déconnexion
+		//c.disconnect(false);
+			
+		
+	}
+	
+	public static void display(Connection connection) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException, IOException, CardException{
+        boolean keep ;
+        do {
+            menu();
+            int userChoice = getChoice() ;
+            keep = handleUserChoice(userChoice,connection);
+        } while (keep) ;
+    }
+
+    public static void menu() {
+        System.out.println("----------------------------MENU-------------------------------");
+        System.out.println("1- Add new user and card");
+        System.out.println("2- Search for a user");
+        System.out.println("3- Delete a user");
+        System.out.println("4- Get the card public key");
+        System.out.println("5- test");
+        System.out.println("6- Exit");
+        System.out.println("7- Auth");
+        return;
+    }
+    public static int getChoice () {
+        System.out.println("\n\n");
+        int i ;
+        while (true) {
+            System.out.print("Enter your choice (Please enter a number between 1 and 5): ");
+            i = myObj.nextInt() ;
+            myObj.nextLine();
+            if (i<=10 && i>=1) {
+                break;
+            }
+            System.out.print("\n");
+
+        }
+        return i ;
+    }
+
+    public static boolean handleUserChoice (int choice , Connection connection) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException, IOException, CardException{
+        switch (choice) {
+            case 1 :
+				handleAddUser(connection);
+                break ;
+            case 2 :
+                handleSearchUser(connection);
+                break ;
+            case 3 :
+                handleDeleteUser(connection) ;
+                break ;
+            case 4 :
+            	
+            	// ********************************************
+                //cad = connectAndSelect();
+            	// ********************************************
+            	
+        		//handleDisplayUsers(connection);
+            	cadPair = APDUOps.connectAndSelect(cad);
+            	canal = cadPair.getKey();
+            	c = cadPair.getValue();
+                respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_SC_RSA_CARD_PUBLIC_MOD, (byte)0x00, (byte)0x00, canal);
+            	DH.printByteArray(respApdu.getData());
+            	
+            	break;
+            
+            // tests
+            case 5:
+            	System.out.print("base: ");
+                Integer base = myObj.nextInt() ;
+                System.out.print("\n");
+
+                System.out.print("power: ");
+                Integer power = myObj.nextInt() ;
+                System.out.print("\n");
+
+                System.out.print("modular: ");
+                Integer mod = myObj.nextInt() ;
+                System.out.print("\n");
+                
+                byte[] baseArray = BigInteger.valueOf(base).toByteArray();
+                byte[] powerArray = BigInteger.valueOf(power).toByteArray();
+                byte[] modArray = BigInteger.valueOf(mod).toByteArray();
+                byte[] result = DH.modularExponentiation(baseArray, powerArray, modArray);
+                System.out.println("base: " + baseArray[0] + "power: " + powerArray[0] + "mod: " + modArray[0]  + "result: " + result[0]);
+                break;
+            case 6 :
+                handleExit(connection) ;
+                break;
+            
+            case 7: 
+            	// authentication
+            	// generation of DH public key
+            	byte[] P = DH.generateRandomPrime(1024).toByteArray();
+            	
+            	// **************************** CLIENT terminal *************************************
+            	
+            	// send P to the card and get the A
+            	cadPair = APDUOps.connectAndSelect(cad);
+            	canal = cadPair.getKey();
+            	c = cadPair.getValue();	
+            	respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_DH_PUBLIC_KEY, (byte)0x00, (byte)0x00, P, canal);
+            	// retreiving A = g^^n mod P
+            	            	
+            	// **************************** SERVER ***********************************
+            	
+            	// generate random private m
+            	byte[] m = DH.generateRandom(1024).toByteArray();
+            	byte[] B = DH.modularExponentiation(G, m, P);
+            	// getting the A from the card
+            	//byte[] A = respApdu.dataOut;
+            	
+            	// simulating the A
+            	byte[] n = DH.generateRandom(1024).toByteArray();
+            	byte[] A = DH.modularExponentiation(G, n, P);
+            	
+            	
+            	// sending B to the card
+            	respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_DH_B, (byte)0x00, (byte)0x00, B, canal);
+            	// getting the card number from the card
+            	long cardNumber;
+            	System.out.println("card heeeere: apdu: ");
+            	System.out.println(respApdu);
+            	cardNumber = DH.byteArrayToLong(respApdu.getData());
+            	
+            	System.out.println("card UID: " + cardNumber);
+            	System.out.println("the m: ");
+            	DH.printByteArray(m);
+            	System.out.println("the B: ");
+            	DH.printByteArray(B);
+            	System.out.println("the A: ");
+            	DH.printByteArray(A);
+            	System.out.println("the P: ");
+            	DH.printByteArray(P);
+            	
+            	// create digital signature for A and B
+            	
+            	// getting private key modulus and exponent from the data base
+            	byte[] privateKey = MyJDBC.getClientData(connection, cardNumber).getServerPrivateKey();
+            	byte[][] concatenatedPrivateKey= RSAOps.separateNAndD(privateKey, MODULUS_SIZE); 
+            
+            	// start signing
+            	byte[] sign = DH.signData(addArrays(A, B), RSAOps.createPrivateKey(concatenatedPrivateKey[0], concatenatedPrivateKey[1]));
+            	
+            	// find AES key
+            	byte[] K = DH.modularExponentiation(A, m, P);
+            	
+            	
+            	
+            	
+            	
+            	
+            	break;
+            
+        }
+        return true ;
+    }
+
+    public static void handleAddUser (Connection connection) throws SQLException, IOException, NoSuchAlgorithmException, InvalidKeySpecException, CardException {
+
+        Random random = new Random();
+        long userCardNumber = Math.abs(random.nextLong());
+        while (MyJDBC.searchClient(connection,userCardNumber)) {
+            userCardNumber = Math.abs(random.nextLong());
+        }
+
+        System.out.println("Card Number was generated successfully...");
+        System.out.println("Please provide the informations we are asking below");
+        System.out.print("Enter the user first name: ");
+        String userFirstName = myObj.nextLine() ;
+        System.out.print("\n");
+
+        System.out.print("Enter the user last name: ");
+        String userLastName = myObj.nextLine() ;
+        System.out.print("\n");
+
+        System.out.print("Enter the user address: ");
+        String useraddress = myObj.nextLine() ;
+        System.out.print("\n");
+
+        // saving information to the database ------------------------------------------------------------
+        
+        cadPair = APDUOps.connectAndSelect(cad);
+    	canal = cadPair.getKey();
+    	c = cadPair.getValue();
+
+        // generating rsa keys ----------------------------------------------------
+		// card keys
+		RSAOps cardRSA = new RSAOps(1024);
+		// private
+		byte[] cardPrivateExp = cardRSA.getPrivateKeyExponent();
+		byte[] cardPrivateP = cardRSA.getPrivateKeyP();
+		byte[] cardPrivateQ = cardRSA.getPrivateKeyQ();
+		// public
+		byte[] cardPublicExp = cardRSA.getPublicKeyExponent();
+		byte[] cardPublicMod = cardRSA.getPublicKeyMod();
+		
+		
+		
+		
+		
+		// server keys
+		RSAOps serverRSA = new RSAOps(1024);
+		// private
+		byte[] serverPrivateExp = serverRSA.getPrivateKeyExponent();
+		byte[] serverPrivateMod = serverRSA.getPrivateKeyMod();
+		byte[] serverPrivateP = serverRSA.getPrivateKeyP();
+		byte[] serverPrivateQ = serverRSA.getPrivateKeyQ();
+		// public
+		byte[] serverPublicExp = serverRSA.getPublicKeyExponent();
+		byte[] serverPublicMod = serverRSA.getPublicKeyMod();
+		
+        //sending keys to the card
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_CARD_PRIVATE_EXP, (byte)0x00, (byte)0x00, cardPrivateExp, canal);
+		
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_CARD_PRIVATE_P, (byte)0x00, (byte)0x00, cardPrivateP, canal);
+		
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_CARD_PRIVATE_Q, (byte)0x00, (byte)0x00, cardPrivateQ, canal);
+		
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_CARD_PUBLIC_EXP, (byte)0x00, (byte)0x00, cardPublicExp, canal);
+		
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_CARD_PUBLIC_MOD, (byte)0x00, (byte)0x00, cardPublicMod, canal);
+		
+		// server public keys
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_SERVER_PUBLIC_EXP, (byte)0x00, (byte)0x00, serverPublicExp, canal);
+		
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_CS_RSA_SERVER_PUBLIC_MOD, (byte)0x00, (byte)0x00, serverPublicMod, canal);
+		
+		// sending the UID of the card
+		respApdu = APDUOps.sendApduToCard(CLA_APPLET, INS_SC_UID, (byte)0x00, (byte)0x00, DH.longToBytes(userCardNumber), canal);
+		
+		
+        // saving all information to the database
+        Client client = new Client(userCardNumber,userFirstName , userLastName , useraddress) ;
+        // setting the keys
+        client.setExpiringDate();
+        client.setPublicKey(concatenateByteArrays(cardPublicMod, cardPublicExp));
+        client.setServerPrivateKey(concatenateByteArrays(serverPrivateMod, serverPrivateExp));
+        MyJDBC.addClient(connection,client);
+        
+        // saving information to the smart card-------------------------------------------------------------
+        
+        System.out.println("User was added successfully");
+        
+        // shutdown the card
+        
+
+    }
+
+    public static void handleSearchUser (Connection connection) throws SQLException {
+        Long cardNumber = getCardNumber() ;
+        System.out.print("\n");
+
+        if (MyJDBC.searchClient(connection,cardNumber)) {
+            System.out.println("User exists and has a card");
+            // display user data
+        }
+        else {
+            System.out.println("Number doesn't represent any card");
+        }
+
+    }
+
+    public static void handleDeleteUser(Connection connection) throws SQLException {
+
+        Long cardNumber = getCardNumber() ;
+        System.out.print("\n");
+        Client client = MyJDBC.getClientData(connection,cardNumber);
+        if (client != null) {
+            System.out.println("User exists and has a card");
+            // display user printUserData(resultSet.getResultSet());
+            System.out.print("Are you sure you want to delete user (y/n): ");
+            String decision = myObj.next();
+            if (decision.equals("y")) {
+                MyJDBC.deleteClient(connection, cardNumber);
+                System.out.println("Client deleted succesfully");
+            } else {
+                System.out.println("Delete Cancelled...");
+            }
+        }
+        else {
+            System.out.println("Number doesn't represent any card");
+        }
+
+    }
+
+
+    public static void handleDisplayUsers(Connection connection) {
+
+    }
+
+    /* public static void printUserData(ResultSet resultSet) throws SQLException {
+        long cardNumber = 0  ;
+        Date expiringDate = null ;
+        String userFirstName = null ;
+        String userlastName = null ;
+        cardNumber = resultSet.getLong("card_number");
+        userFirstName = resultSet.getString("first_name");
+        userlastName = resultSet.getString("last_name");
+
+
+
+        System.out.println("--------------------------------------------------------------------");
+
+            // Concatenate user data in one line
+        String userData = String.format("%-18d| %-25s|%-25s%n", cardNumber, userFirstName, userlastName);
+        System.out.println(userData);
+        System.out.println("--------------------------------------------------------------------");
+    } */
+
+    public static void handleExit(Connection connection) {
+
+        System.out.print("Are you sure you want to exit program (y/n): ");
+        String decision = myObj.next();
+        if (decision.equals("y")) {
+            MyJDBC.closeConnection(connection);
+            System.out.println("EXITING PROGRAM...");
+
+            System.exit(0);
+        }
+    }
+
+    public static long getCardNumber() {
+        System.out.print("Enter the card Number: ");
+        return Long.valueOf(myObj.nextLong());
+
+    }
+    
+    
+	
+	// concatenate byte arrays
+	public static byte[] concatenateByteArrays(byte[] a, byte[] b) throws IOException {
+	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	    outputStream.write(a);
+	    outputStream.write(b);
+	    return outputStream.toByteArray();
+	}
+	
+	
+	// addition of two byte arrays
+	public static byte[] addArrays(byte[] array1, byte[] array2) {
+        // Check if both arrays have the same length
+        if (array1.length != array2.length) {
+            throw new IllegalArgumentException("Arrays must have the same length");
+        }
+
+        // Create a new array to store the result
+        byte[] result = new byte[array1.length];
+
+        // Perform addition of corresponding elements
+        for (int i = 0; i < array1.length; i++) {
+            // Add the elements of the two arrays at index i
+            result[i] = (byte) (array1[i] + array2[i]);
+        }
+
+        return result;
+    }
+
+}
